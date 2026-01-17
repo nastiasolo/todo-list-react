87004e19fcbe284ba784692fd5e33db248cb8cbc
nastiasolo <ag.solodova@gmail.com>
2026-01-17 22:15:41 +0100

    initial commit: change previous project to React

---
+++ src/components/Todo.jsx
@@ -0,0 +1,26 @@
+import AddTaskForm from "./AddTaskForm";
+import SearchTaskForm from "./SearchTaskForm";
+import TodoInfo from "./TodoInfo";
+import TodoList from "./TodoList";
+
+const Todo = () => {
+  const tasks = [
+    { id: "task-1", title: "Buy milk", isDone: false },
+    { id: "task-2", title: "Do homework", isDone: true },
+  ];
+
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm />
+      <SearchTaskForm />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+      />
+      <TodoList tasks={tasks} />
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,28 @@
+import TodoItem from "./TodoItem";
+
+const TodoList = (props) => {
+  const { tasks = [] } = props;
+
+  const hasTasks = true;
+
+  if (!hasTasks) {
+    return <div className="todo__empty-message"></div>;
+  }
+
+  return (
+    <ul className="todo__list">
+      {tasks.map((task) => (
+        <TodoItem
+          className="todo__item"
+          // id={task.id}
+          // title={task.title}
+          // isDone={task.isDone}
+          key={task.id}
+          {...task}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
