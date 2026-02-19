0014642981f85d3629a0b098fe93b3a30d29d1f7
nastiasolo <ag.solodova@gmail.com>
2026-02-19 12:51:02 +0100

    router and link router

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import { memo, useContext } from "react";
 import { TasksContext } from "../context/TasksContext";
+import RouterLink from "./RouterLink";
 
 const TodoItem = (props) => {
   const { className = "", id, title, isDone } = props;
@@ -25,9 +26,12 @@ const TodoItem = (props) => {
           toggleTaskComplete(id, target.checked);
         }}
       />
-      <label className="todo-item__label" htmlFor={id}>
+      <label className="todo-item__label visually-hidden" htmlFor={id}>
         {title}
       </label>
+      <RouterLink to={`/tasks/${id}`} aria-label="Task detail page">
+        {title}
+      </RouterLink>
       <button
         className="todo-item__delete-button"
         aria-label="Delete"
